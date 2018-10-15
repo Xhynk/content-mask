bdd8ac7b2be56b30776d64b1f171fa9b3e88f2ae
alexdemchak <alexdemchak@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2018-10-15 22:44:08 +0000

    Minimized admin.js file for end-use
    git-svn-id: https://plugins.svn.wordpress.org/content-mask/trunk@1957176 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -1,4 +1,7 @@
 jQuery(document).ready(function($){
+	/**
+	 * Meta Box Change Functions
+	 */
 	$('#content_mask_meta_box [name="content_mask_method"]').on('change', function(){
 		if( $(this).val() == 'download' ){
 			$('.content-mask-expiration-div').fadeIn();
@@ -7,8 +10,50 @@ jQuery(document).ready(function($){
 		}
 	});
 
+	if( $('#content_mask_enable').is(':checked') ){
+		$('#postdivrich').css({'height': 0, 'overflow': 'hidden'}).addClass('hide-overflow');
+	}
+
+	$('#content-mask-settings').on('click', '.content-mask-check', function(){
+		if( !$(this).is(':checked') ){
+			$('#postdivrich').animate({'height': 416, 'overflow': 'visible'}).removeClass('hide-overflow');
+			$('.gutenberg').addClass('content-mask-unchecked');
+			$('.gutenberg .edit-post-visual-editor, .gutenberg .edit-post-text-editor').fadeIn();
+			$('.content-mask-notice').fadeOut();
+		}
+	});
+
+	/**
+	 * Insert Notices to Alert User of Actions
+	 */
 	function contentMaskMessage( classes, message ){
-		$('.toplevel_page_content-mask #wpbody .wrap > h2').after('<div class="content-mask-message notice notice-'+ classes +'"><p>'+ message +'</p></div>');
+		$('#content-mask-message').remove(); //Prevent Duplicates
+
+		$('body').append('<div id="content-mask-message"><div class="content-mask-message-content '+ classes +'">'+ message +'</div></div>');
+		setTimeout(function(){
+			$('#content-mask-message').fadeOut(function(){
+				$(this).remove();
+			});
+		}, 1500);
+	}
+
+	/**
+	 * Control Modal (currently only for Delete Mask)
+	 */
+	function insertContentMaskModal( postID, title, classes ){
+		$('#content-mask-modal-container').remove(); // Prevent Duplicates
+
+		var modal = '<div id="content-mask-modal-container">'+
+    					'<div class="content-mask-modal '+ classes +'">'+
+							'<svg class="icon content-mask-svg svg-trash" title="Delete Mask" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><polyline points="3 6 5 6 21 6"></polyline><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path><line x1="10" y1="11" x2="10" y2="17"></line><line x1="14" y1="11" x2="14" y2="17"></line></svg>'+
+							'<h2>'+ title +'</h2>'+
+							'<p>Remove the Content Mask from this?</p>'+
+							'<button data-intent="confirm" data-action="delete-mask" data-id="'+ postID +'">Yes</button><button data-intent="cancel">No</button>'+
+						'</div>'+
+					'</div>';
+
+		$('body').addClass('blur');
+		$('body').append( modal );
 	}
 
 	$('#content-mask-pages').on( 'click', '.refresh-transient', function(){
@@ -39,7 +84,7 @@ jQuery(document).ready(function($){
 			if( response.status == 200 ){
 				$('#content-mask-pages tr td.status').each(function(){
 					if( $(this).closest('tr').find('.info .meta a').text() == maskURL ){
-						$(this).find('.transient-expiration').text( expirationReadable );
+						$(this).find('.transient-expiration').removeClass('expired').text( expirationReadable );
 					}
 				});
 				classes = 'info';
@@ -54,40 +99,66 @@ jQuery(document).ready(function($){
 		return false;
 	});
 
+	/**
+	 * Insert Popup for Delete Mask
+	 */
 	$('#content-mask-pages').on( 'click', '.remove-mask', function(){
-		if( confirm( 'Are you sure you want to remove the Content Mask from '+ $(this).closest('tr').find('.info strong').text() +'?' ) ){
-			$('.content-mask-message').remove();
+		var	$clicked   = $(this),
+			$row       = $(this).closest('tr'),
+			postID     = $clicked.closest('tr').attr('data-attr-id'),
+			title      = $clicked.closest('tr').find('.info strong').text();
+		
+		insertContentMaskModal( postID, title, 'warning' );
 
-			var	$clicked   = $(this),
-				$row       = $(this).closest('tr');
-				postID     = $clicked.closest('tr').attr('data-attr-id');
-
-			var data = {
-				'action': 'delete_content_mask',
-				'postID': postID
-			};
-
-			$row.addClass('deleting');
-
-			$.post(ajaxurl, data, function(response) {
-				$('.content-mask-message').remove(); // Prevent weird interaction with existing messages
-				var classes;
-
-				if( response.status == 200 ){
-					$row.fadeOut();
-					classes = 'info';
-				} else if( response.status == 400 || response.status == 403 ){
-					$row.addClass('deleting');
-					classes = 'error';
-				}
-				
-				contentMaskMessage( classes, response.message );
-			}, 'json');
-
-			return false;
-		}
+		return false;
 	});
 
+	/**
+	 * Handle Delete Mask Intent
+	 */
+	$('body').on('click', '[data-intent="cancel"]', function(){
+		$('body').removeClass('blur');
+		$('#content-mask-modal-container').fadeOut(function(){
+			$(this).remove();
+		});
+		return false;
+	});
+	$('body').on('click', '[data-action="delete-mask"]', function(){
+		var	$clicked   = $(this),
+			postID     = $clicked.attr('data-id'),
+			$row       = $('#content-mask-pages').find('tr[data-attr-id="'+ postID +'"]');
+
+		var data = {
+			'action': 'delete_content_mask',
+			'postID': postID
+		};
+
+		$('body').removeClass('blur');
+		$('#content-mask-modal-container').fadeOut(function(){
+			$(this).remove();
+			$row.addClass('deleting');
+		});
+
+		$.post(ajaxurl, data, function(response) {
+			var classes;
+
+			if( response.status == 200 ){
+				$row.fadeOut();
+				classes = 'info';
+			} else if( response.status == 400 || response.status == 403 ){
+				$row.addClass('deleting');
+				classes = 'error';
+			}
+			
+			contentMaskMessage( classes, response.message );
+		}, 'json');
+
+		return false;
+	});
+
+	/**
+	 * Toggle Enabled/Disabled State
+	 */
 	$('#content-mask-pages, #the-list .column-content-mask').on( 'click', '.method div, .content-mask-method svg', function(){
 		var	$clicked     = $(this),
 			restoreIcon  = $clicked.attr('class'),
@@ -117,7 +188,6 @@ jQuery(document).ready(function($){
 		};
 
 		$.post(ajaxurl, data, function(response) {
-			$('.content-mask-message').remove(); // Prevent weird interaction with existing messages
 			var classes;
 
 			if( response.status == 200 ){
@@ -134,11 +204,15 @@ jQuery(document).ready(function($){
 		}, 'json');
 	});
 
-	$('#content-mask-options .content-mask-checkbox').on( 'click', '.content-mask-check', function(){
-		var	$clicked     = $(this).closest('.content-mask-checkbox'),
-			currentState = $clicked.attr('data-attr'),
-			optionName   = $clicked.find('input[type="checkbox"]').attr('name'),
-			optionDisplayName = $clicked.find('.display-name').attr('aria-label').replace('Enable ', '');
+	/**
+	 * Toggle General Options
+	 */
+	$('.content-mask-admin .content-mask-checkbox').on( 'click', '.content-mask-check', function(){
+		var	$clicked     = $(this),
+			$label       = $clicked.closest('label'),
+			currentState = $label.attr('data-attr'),
+			optionName   = $label.find('input[type="checkbox"]').attr('name'),
+			optionDisplayName = $label.find('.display-name').attr('aria-label').replace('Enable ', '');
 
 		var data = {
 			'action': 'toggle_content_mask_option',
@@ -147,10 +221,9 @@ jQuery(document).ready(function($){
 			'optionDisplayName': optionDisplayName,
 		};
 
-		$clicked.closest('.content-mask-option').addClass('content-mask-reloading');
-		$.post(ajaxurl, data, function(response) {
-			$('.content-mask-message').remove(); // Prevent weird interaction with existing messages
-			
+		$clicked.addClass('content-mask-reloading');
+
+		$.post(ajaxurl, data, function(response) {			
 			if( response.status == 200 ){
 				classes = 'info';
 			} else if( response.status == 400 || response.status == 403 ){
@@ -162,19 +235,20 @@ jQuery(document).ready(function($){
 				$('#content-mask-list').addClass('tracking-' + response.newState);
 			}
 
-			$clicked.closest('.content-mask-option').removeClass('content-mask-reloading');
+			$clicked.removeClass('content-mask-reloading');
 			$clicked.closest('.content-mask-option').find('.content-mask-value').text( response.newState );
-			$clicked.attr('data-attr', response.newState );
+			$label.attr('data-attr', response.newState );
 
 			contentMaskMessage( classes, response.message );
 		}, 'json');
 	});
 
-	$('#content-mask-code button').on( 'click', function(){
-		$('.content-mask-message').remove();
-
+	/**
+	 * Update Code Editors
+	 */
+	$('#content-mask-advanced button').on( 'click', function(){
 		var	$clicked = $(this),
-			$wrap    = $clicked.closest('.code-edit-wrapper'),
+			$wrap    = $clicked.closest('.option').find('.code-edit-wrapper'),
 			editor   = $clicked.attr('data-editor'),
 			value    = window[editor].codemirror.getValue(),
 			label    = $clicked.text().replace('Save ', '');
@@ -189,9 +263,7 @@ jQuery(document).ready(function($){
 
 		$wrap.addClass('loading');
 
-		$.post(ajaxurl, data, function(response) {
-			$('.content-mask-message').remove(); // Prevent weird interaction with existing messages
-			
+		$.post(ajaxurl, data, function(response) {			
 			if( response.status == 200 ){
 				classes = 'info';
 			} else if( response.status == 400 || response.status == 403 ){
@@ -208,52 +280,62 @@ jQuery(document).ready(function($){
 		}, 'json');		
 	});
 
-	$('#content-mask-list .content-mask-table-body').scroll(function(){
-		var $scrolled = $(this);
-		var tbody = $(this).find('tbody');
+	/**
+	 * Dynamically Load more Content Masked Pages/Posts/CPTS
+	 */
+	$('#content-mask #load-more-masks').on( 'click', function(){
+		var $tbody = $('#content-mask-pages').find('tbody');
 
-		if( ! $scrolled.hasClass('currently-loading') ){
-			if( $(tbody).find('tr').length >= 20 && $(tbody).find('tr:last-child div').text() != 'No More Content Masks Found' ){
-				if( $(this)[0].scrollHeight - $(this).scrollTop() <= $(this).outerHeight() ){
-					$scrolled.addClass('currently-loading');
+		$tbody.append('<tr class="content-mask-temp"><td><div class="content-mask-spinner"><div class="bounce1"></div><div class="bounce2"></div><div class="bounce3"></div></div></td></tr>');
 
-					$(tbody).append('<tr style="position: absolute; bottom: -40px; width: calc(100% - 18px);" class="content-mask-temp"><td><div class="content-mask-spinner"><div class="bounce1"></div><div class="bounce2"></div><div class="bounce3"></div></div></td></tr>');
+		var data = {
+			'action': 'load_more_pages',
+			'offset': $tbody.find('tr').length,
+		};
 
-					var data = {
-						'action': 'load_more_pages',
-						'offset': $(tbody).find('tr').length,
-					};
+		$.post(ajaxurl, data, function(response){
+			$('.content-mask-temp').remove();
+			$tbody.append( response.message );
+			contentMaskMessage( 'info', 'Loading Completed' );
 
-					$.post(ajaxurl, data, function(response){
-						$('.content-mask-temp').html( '<h2><strong>Loading Completed.</strong></h2>' );
-						$(tbody).append( response.message);
-						setTimeout(function(){
-							$('.content-mask-temp').fadeOut();
-							$scrolled.removeClass('currently-loading');
-						}, 250 );
-					}, 'json');
-				}
+			if( response.notice != null && response.notice == 'no remaining' ){
+				$('#load-more-masks').fadeOut(function(){
+					$(this).remove();
+				});
 			}
-		}
+		}, 'json');
 	});
 
-	if( $('#content_mask_enable').is(':checked') ){
-		$('#postdivrich').css({'height': 0, 'overflow': 'hidden'}).addClass('hide-overflow');
-	}
+	/**
+	 * Admin Panel Navigation
+	 */
+	$('#content-mask nav').on('click', 'li', function(){
+		var $link  = $(this).find('a'),
+			target = $link.attr('data-target'); 
+		
+		// Set Active
+		$('#content-mask nav li').each(function(){
+			$(this).find('a').removeClass('active');
+		});
+		$link.addClass('active');
 
-	$('#content-mask-settings').on('click', '.content-mask-check', function(){
-		if( !$(this).is(':checked') ){
-			$('#postdivrich').animate({'height': 416, 'overflow': 'visible'}).removeClass('hide-overflow');
-			$('.gutenberg').addClass('content-mask-unchecked');
-			$('.gutenberg .edit-post-visual-editor, .gutenberg .edit-post-text-editor').fadeIn();
-			$('.content-mask-notice').fadeOut();
-		}
+		// Show/Hide Panels
+		$('#content-mask .content-mask-panel.active').fadeOut(function(){
+			$(this).removeClass('active');
+
+			$('#'+target).fadeIn(function(){
+				$(this).addClass('active');
+			});
+		});
 	});
 
-	$('.collapse-handle').click(function(){
-		$(this).toggleClass('collapsed');
-		$(this).closest('h3').next('.collapse-container').slideToggle();
-	});
+	/**
+	 * Admin Mobile Menu
+	 */
+
+	 $('#mobile-nav-toggle').on('click', function(){
+	 	$('#header-nav').slideToggle();
+	 });
 });
 
 jQuery(window).load(function(){
